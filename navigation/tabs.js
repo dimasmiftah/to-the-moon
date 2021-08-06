ae019cea411e1439355657601fa0152528cfb855
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2021-08-06 16:55:27 +0700

    add bottom tab navigator

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -1,47 +1,89 @@
-import React from "react";
-import {
-    TouchableOpacity,
-} from "react-native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import { Home, Portfolio, Market, Profile } from "../screens"
-import { COLORS } from "../constants"
+import { Home, Portfolio, Market, Profile } from '../screens';
+import { TabIcon } from '../components';
+import { COLORS, icons } from '../constants';
 
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
-
-    return (
-        <Tab.Navigator
-            tabBarOptions={{
-                style: {
-                    backgroundColor: COLORS.primary,
-                    borderTopColor: "transparent",
-                }
-            }}
-        >
-            <Tab.Screen
-                name="Home"
-                component={Home}
-            />
-            <Tab.Screen
-                name="Portfolio"
-                component={Portfolio}
-            />
-            <Tab.Screen
-                name="Trade"
-                component={Home}
-            />
-            <Tab.Screen
-                name="Market"
-                component={Market}
-            />
-            <Tab.Screen
-                name="Profile"
-                component={Profile}
-            />
-        </Tab.Navigator>
-    )
-}
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          height: 140,
+          backgroundColor: COLORS.primary,
+          borderTopColor: 'transparent',
+        },
+      }}
+    >
+      <Tab.Screen
+        name='Home'
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return <TabIcon focused={focused} icon={icons.home} label='Home' />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name='Portfolio'
+        component={Portfolio}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <TabIcon
+                focused={focused}
+                icon={icons.briefcase}
+                label='Portfolio'
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name='Trade'
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <TabIcon
+                focused={focused}
+                icon={icons.trade}
+                label='Trade'
+                isTrade={true}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name='Market'
+        component={Market}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <TabIcon focused={focused} icon={icons.market} label='Market' />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name='Profile'
+        component={Profile}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <TabIcon focused={focused} icon={icons.profile} label='Profile' />
+            );
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default Tabs;
--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import {
-    View,
-    Text
-} from 'react-native';
+import { View, Text } from 'react-native';
 
 const Home = () => {
-    return (
-        <View>
-            <Text>Home</Text>
-        </View>
-    )
-}
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
 
 export default Home;
